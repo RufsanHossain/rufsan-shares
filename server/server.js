69db8ef1fc50f621dc89bcbedc8727be44f2b8d5
Rufsan Hossain Santo <rufsanhossain315@gmail.com>
2024-11-02 05:46:41 +0600

    server setup

---
+++ server/server.js
@@ -0,0 +1,130 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import 'dotenv/config';
+import bcrypt from 'bcrypt';
+import { nanoid } from 'nanoid';
+import jwt from 'jsonwebtoken';
+
+// schema import
+import User from './Schema/User.js';
+
+const server = express();
+let PORT = 3000;
+
+let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; 
+let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/; 
+
+server.use(express.json());
+
+mongoose.connect(process.env.DB_LOCATION, {
+    autoIndex: true
+})
+
+const formatDataToSend = (user) => {
+
+    const access_token = jwt.sign({ id: user._id }, process.env.SECRET_ACCESS_KEY)
+
+    return {
+        access_token,
+        profile_img: user.personal_info.profile_img,
+        username: user.personal_info.username,
+        fullname: user.personal_info.fullname
+    }
+}
+
+const generateUsername = async (email) => {
+    let username = email.split('@')[0];
+    let isUsernameNotUnique = await User.exists({ "personal_info.username": username }).then((result) => result);
+
+    isUsernameNotUnique ? username += nanoid().substring(0, 3) : "";
+
+    return username;
+}
+
+server.post("/signup", (req, res) => {
+    let { fullname, email, password } = req.body;
+
+    // validating the data from frontend
+    if(fullname.length < 3) {
+        return res.status(403).json({ "error": "Fullname must be at least 3 letters long." });
+    }
+
+    // email verifiation
+    if(!email.length) {
+        return res.status(403).json({ "error": "Email is required." });
+    }
+    
+    // email pattern
+    if(!emailRegex.test(email)) {
+        return res.status(403).json({ "error": "Invalid email format." });
+    }
+
+    // password verifiation
+    if(!password.length) {
+        return res.status(403).json({ "error": "Password is required." });
+    }
+
+    // password pattern
+    if(!passwordRegex.test(password)) {
+        return res.status(403).json({ "error": "Password must be at least 6 characters long and contain at least one uppercase letter, one lowercase letter, and one number." });
+    }
+
+    bcrypt.hash(password, 10, async (err, hashed_password) => {
+        let username = await generateUsername(email);
+
+        let user = new User({
+            personal_info: {fullname, email, password: hashed_password, username}
+        })
+
+        user.save().then((u) => {
+            return res.status(200).json(formatDataToSend(u));
+        })
+        .catch(err => {
+            if (err.code == 11000) {
+                return res.status(500).json({ "error": "Email already exists" });
+            }
+            return res.status(500).json({ "error": err.message });
+        })
+        
+        console.log(hashed_password);
+    })
+
+   
+})
+
+server.post("./signin", (req, res) => {
+    let { email, password } = req.body;
+
+    User.findOne({ "personal_info.email": email })
+    .then((user) => {
+
+        if(!user) {
+            return res.status(403).json({ "error": "Email not found" });
+        }
+        
+        bcrypt.compare(password, user.personal_info.password, (err, result) => {
+
+            if(err) {
+                return res.status(403).json({ "error": "Error ocurred while logging in. Please try again." });
+            }
+
+            if (!result) {
+                return res.status(403).json({ "error": "Incorrect password" });
+            } else {
+                return res.status(200).json(formatDataToSend(user));
+            }
+
+        })
+
+    })
+    .catch(err => {
+
+        console.log(err.message);
+        return res.status(500).json({ "error": err.message });
+
+    })
+})
+
+server.listen(PORT, () => {
+    console.log('listening on port -> ' + PORT);
+});
